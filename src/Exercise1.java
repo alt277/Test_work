b8077c5f5582fd5bdeae8d65eebc03b8d8655f41
alt277 <altima875@gmail.com>
2021-10-24 13:01:48 +0300

    Exercise1 done

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,2 +1,22 @@
 public class Exercise1 {
+
+    public static String myreplace(String string, char oldChar, char newChar) {
+        String newWord = "";
+        char current;
+        for (int i = 0; i <= string.length() - 1; i++) {
+            current = string.charAt(i);
+            if (current == oldChar)
+                newWord += newChar;
+            else
+                newWord += current;
+        }
+        return newWord;
+    }
+
+
+    public static void main(String[] args) {
+        String word = "Hello world";
+        String newWord = myreplace(word, 'l', 'w');
+        System.out.println("New word : " + newWord);
+    }
 }
